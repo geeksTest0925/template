087d22adee3595b7ba04079329adac4146bb31ba
junnan fu <wb_003@hereto.cn>
2023-02-21 17:21:40 +0800

    update

--- src/mock/side-menu.js
+++ src/mock/side-menu.js
@@ -4,14 +4,12 @@ export const mockMenuDatas = [
         id: 1,
         name: "首页",
         icon: "InboxOutlined",
-        hidden: false,
-        alias: 'HOME_DETAIL',
+        alias: 'HOME_DETAIL'
     },
     {
         id: 2,
         name: "我的信息",
         icon: "InboxOutlined",
-        hidden: false,
         alias: 'MY_INFO_DETAIL'
     },
 ];
