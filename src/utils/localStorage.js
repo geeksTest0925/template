0597e0650e8f2f2d57e229c1ee299fa6a4897bce
junnan fu <wb_003@hereto.cn>
2023-02-22 15:04:30 +0800

    update

---
+++ src/utils/localStorage.js
@@ -0,0 +1,18 @@
+const pre = 'LJYT_';
+let localS = {
+    save (key, value) {
+        const v = value || ''
+        localStorage.setItem(pre+key, JSON.stringify(v))
+    },
+    get (key, defaultValue = '') {
+        return JSON.parse(localStorage.getItem(pre+key)) || defaultValue
+    },
+    remove (key) {
+        localStorage.removeItem(pre+key)
+    },
+    clear () {
+        localStorage.clear()
+    }
+}
+
+export default localS
